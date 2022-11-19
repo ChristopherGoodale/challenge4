58ab6b6f5b67b7f9993d939e9ebd563684dcbdc3
ChristopherGoodale <christophergdale@gmail.com>
2022-11-19 17:31:57 -0600

    needed changes to javascript

--- asset/javascript.js
+++ asset/javascript.js
@@ -1,1 +1,17 @@
+//global variables
+let deadline = new Date().getTime(30000);
+
+let now = new Date().getTime();
+let t = deadline - now;
+//functions
+let timer = setInterval(function () {
+
+}, 1000);
+//executables
+
+//documents
+document.getElementById("timer-days").innerHTML = days + "<span class='label'>DAY(S)</span>";
+document.getElementById("timer-hours").innerHTML = ("0" + hours).slice(-2) + "<span class='label'>HR(S)</span>";
+document.getElementById("timer-mins").innerHTML = ("0" + mins).slice(-2) + "<span class='label'>MIN(S)</span>";
+document.getElementById("timer-secs").innerHTML = ("0" + secs).slice(-2) + "<span class='label'>SEC(S)</span>";
 
