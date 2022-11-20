b778d8d06b35e36c9a91424243983fbf5b54e9b2
ChristopherGoodale <christophergdale@gmail.com>
2022-11-19 20:18:11 -0600

    These commits need to be made for javascirpt

--- asset/javascript.js
+++ asset/javascript.js
@@ -1,17 +1,25 @@
 //global variables
-let deadline = new Date().getTime(30000);
+let now = new Date().now();
+let endTime = now.setInterval();
 
-let now = new Date().getTime();
 let t = deadline - now;
 //functions
 let timer = setInterval(function () {
+    let now = newDate().getTime();
+    let t = endDate - now;
+
+    if (t >= 0) {
+        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor((t % (1000 * 90)) / 1000);
+        document.getElementById("timer-mins").innerHTML = ("0" + mins).slice(-2) + "<span class='label'>MIN(S)</span>";
+        document.getElementById("timer-secs").innerHTML = ("0" + secs).slice(-2) + "<span class='label'>SEC(S)</span>";
+    } else {
+        document.getElementById("timer").innerHTML = "The countdown is over!"
+    }
 
 }, 1000);
 //executables
 
 //documents
-document.getElementById("timer-days").innerHTML = days + "<span class='label'>DAY(S)</span>";
-document.getElementById("timer-hours").innerHTML = ("0" + hours).slice(-2) + "<span class='label'>HR(S)</span>";
-document.getElementById("timer-mins").innerHTML = ("0" + mins).slice(-2) + "<span class='label'>MIN(S)</span>";
-document.getElementById("timer-secs").innerHTML = ("0" + secs).slice(-2) + "<span class='label'>SEC(S)</span>";
+
 
