c028d181163e669ecb08dd8d55d72385ae634be8
ChristopherGoodale <christophergdale@gmail.com>
2022-11-20 22:00:56 -0600

    changes to javascript and css

--- asset/javascript.js
+++ asset/javascript.js
@@ -1,58 +1,93 @@
 //global variables
-let startQuizbtn =
-    let now = new Date().now();
-let endTime = now.setInterval();
+
+let saveScore = document.querySelector("#saveInitials");
+let showScores = document.querySelector("#showScores");
+let viewScores = document.querySelector("#viewScores");
+let startQuizbtn = document.querySelector("#startQuiz");
+let questionSection = document.querySelector("#questions");
 let questions = [
-    { title: ["Question 1"], choices: ["hi", "how", "are", "you"], answer: ["hi"] },
-    { title: ["This is the first question"], choices: ["hi", "how", "are", "you"], answer: ["hi"] },
-]
+    { title: ["Which of the below options is not a language we have coded in?"], choices: ["python", "css", "html", "javascript"], answer: ["python"] },
+    { title: ["What is the correct character to use when calling IDs?"], choices: [".", "!", "4", "#"], answer: ["#"] },
+    { title: ["what language, now deprecated, allowed coders to quickly set up websites with out-of-the-box designs?"], choices: ["python", "jquery", "tailwind", "css"], answer: ["jquery"] },
+    { title: ["This is the fourth question"], choices: ["a", "b", "c", "d"], answer: ["c"] },
+    { title: ["This is the fifth question"], choices: ["a", "b", "c", "d"], answer: ["b"] },
+    { title: ["This is the sixth question"], choices: ["a", "b", "c", "d"], answer: ["b"] },
+];
+let questionsIndex = 1;
+let timerDiv = document.querySelector("#timer");
+let score = 0;
+let time = 60
+let timer = 0;
+
+let scores = JSON.parse(localStorage.getItem("scores")) || [];
 
 //functions
 function startQuiz() {
+    timer = setInterval(function () {
+        time--; ``
+        timerDiv.innerHTML = time;
+    }, 1000);
+    alert("Begin game")
     //aler("i started the game");
-
+    createButtons(0)
     //bring up question
-    let title = document.createElement("h2");
-    title.textContent = questions[0].choices[0];
-    qDiv.appendChild(title);
 
-    let buttonOne = document.createElement("button");
-    buttonOne.textContent = questions[0].choices[0];
-    buttonOne.dataset.answer = questions[0].answer;
-    qDiv.appendChild(buttonOne);
-
-    let buttonTwo = document.createElement("button");
-    buttonTwo.textContent = questions[0].choices[1];
-    buttonTwo.dataset.answer = questions[0].answer;
-    qDiv.appendChild(buttonTwo);
-
-    let buttonThree = document.createElement("button");
-    buttonThree.textContent = questions[0].choices[2];
-    buttonThree.dataset.answer = questions[0].answer;
-    qDiv.appendChild(buttonThree);
-
-    let buttonFour = document.createElement("button");
-    buttonFour.textContent = questions[0].choices[3];
-    buttonFour.dataset.answer = questions[0].answer;
-    qDiv.appendChild(buttonFour);
 
     //make clickable
 }
 
+function createButtons(index) {
+    //this will cycle the buttons 
+    let title = document.createElement("h2");
+    title.textContent = questions[index].title;
+    questionSection.appendChild(title);
+
+    //buttons
+    let buttonOne = document.createElement("button");
+    buttonOne.textContent = questions[index].choices[0];
+    buttonOne.dataset.answer = questions[index].answer;
+    questionSection.appendChild(buttonOne);
+
+    let buttonTwo = document.createElement("button");
+    buttonTwo.textContent = questions[index].choices[1];
+    buttonTwo.dataset.answer = questions[index].answer;
+    questionSection.appendChild(buttonTwo);
+
+    let buttonThree = document.createElement("button");
+    buttonThree.textContent = questions[index].choices[2];
+    buttonThree.dataset.answer = questions[index].answer;
+    questionSection.appendChild(buttonThree);
+
+    let buttonFour = document.createElement("button");
+    buttonFour.textContent = questions[index].choices[3];
+    buttonFour.dataset.answer = questions[index].answer;
+    questionSection.appendChild(buttonFour);
+}
 
 startQuizbtn.addEventListener("click", startQuiz);
 
-qDiv.addEventListener("click", function (event) {
+questionSection.addEventListener("click", function (event) {
     console.log(event);
     let choice = event.target.innerHTML;
     let answer = event.target.dataset.answer;
     if (choice === answer) {
         alert('you are correct');
-        Score++;
+        if (questionsIndex === questions.length) {
+            alert('Game over!')
+            score++;
+        } else
+            createButtons(questionsIndex);
+        questionsIndex++;
+        score++;
+        console.log(score);
     } else {
-        alert('incorrect');
+        if (questionsIndex === questions.length) {
+            alert('Game over!')
+        } else
+            alert('Oops, that\'s wrong!');
+        createButtons(questionsIndex);
+        console.log(score);
     }
-
     //go to next question
 }
 )
